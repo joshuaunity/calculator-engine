a61108fa9a478e247e40abf3f6ec05ae57846d18
joshuaunity <oghenerobojosh01@gmail.com>
2023-11-08 20:02:30 +0100

    feat: couple of logics
    - logic for addition
    - logic to clean tokens of unwanted charaters and validation error
    - logic to process token into numbers

--- engine.py
+++ engine.py
@@ -1,29 +1,58 @@
 class CalEngine:
-    def __init__(self, tokens):
-        self.tokens = tokens
+    # def __init__(self, tokens):
+    #     self.tokens = tokens
 
 
     def process_tokens(self, tokens):
         # result = tokens.find()
         pass
+    
+
+    def clean_input(self, tokens):
+        tokens = tokens.lower().replace(" ", "")
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        for e in tokens:
+            if e in letters:
+                # raise error
+                return {"error": "Invalid Input"}
+        return tokens
+    
+
+    def convert_tokens(self, tokens, operator, d_type=float):
+        # split tokens into a list
+        tokens = tokens.split(tokens, operator)
+        # convert the values from string to floats
+        values = [d_type(i) for i in tokens]
+        return values
+
 
     def process_brackets(self, tokens):
         indexes = []
-        index = tokens
-        pass 
+        for e in tokens:
+            if e == "(" or e == ")":
+                indexes.append(self, tokens.index(e))
+        
 
-    def division(self, values):
-        # value = 
+    def addition(self, tokens):
+        # split tokens into a list and convert to floats
+        values = self.convert_tokens(tokens, "+")
+        total = sum(values)
+        return total
+
+
+    def multiplication(self, tokens):
+        # split tokens into a list and convert to floats
+        values = self.convert_tokens(tokens, "*")
+        total
+
+
+    def division(self, tokens):
         pass
 
-    def multiplication(self):
+
+    def subtraction(self, tokens):
         pass
 
-    def addition(self):
-        pass
-
-    def subtraction(self):
-        pass
 
 
 
@@ -35,3 +64,6 @@ class CalEngine:
 # M - Multiplication - 2*3
 # A - Addition - 2+3
 # S - Subtraction - 2-3
+
+cal_engine = CalEngine()
+print(CalEngine.clean_input("jfndof()jfe(k fmsko23()"))
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-tokens = "jfndof()jfe(kfmsko23()"
+tokens = "0+4+5+6+7+8+9+10+11+12+13+14+15"
 # loop through the string
+# if the character is a ( or a ) then add it to a list
+# if the character is not a ( or a ) then add it to a string
+# if the character is a ( or a ) then add the string to the list
 
+# for e in tokens:
+#     print(e, tokens.index(e))
+
+    # if e == "(" or e == ")":
+    #     print(e)
+    # else:
+    #     print("not a parenthesis")
 
 # for e in tokens
 
 
-
-print(tokens.split(""))
+print(tokens.split("+"))
