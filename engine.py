83215b42ba30cf4c09a2f216615f531fd3b658ea
joshuaunity <oghenerobojosh01@gmail.com>
2023-11-08 14:43:34 +0100

    chore: initialization

---
+++ engine.py
@@ -0,0 +1,18 @@
+class CalEngine:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+
+    def process_tokens(self):
+        pass
+
+
+
+# BODMAS
+
+# B - Brackets - ()
+# O - Orders - Exponents - 2^3
+# D - Division - 2/3
+# M - Multiplication - 2*3
+# A - Addition - 2+3
+# S - Subtraction - 2-3
