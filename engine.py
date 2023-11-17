1b0156e64ef569434b67247c01aa28555c52cc8f
joshuaunity <oghenerobojosh01@gmail.com>
2023-11-17 13:46:31 +0100

    chore: initialize dev branhc

--- engine.py
+++ engine.py
@@ -2,11 +2,10 @@ class CalEngine(object):
     # def __init__(self, tokens):
     #     self.tokens = tokens
 
-
     def process_tokens(self, tokens):
         # result = tokens.find()
         pass
-    
+
     @staticmethod
     def clean_input(tokens):
         tokens = tokens.lower().replace(" ", "")
@@ -15,15 +14,22 @@ class CalEngine(object):
             if e in letters:
                 # raise error
                 return {"error": "Invalid Input"}
+        # convert tokens to a list
+        tokens = list(tokens)
+        # loop through the tokens and convert any numbers to float
+        for token in tokens:
+            if token.isdigit():
+                tokens[tokens.index(token)] = float(token)
         return tokens
-    
+
     @classmethod
-    def convert_tokens(self, tokens, operator, d_type=float):
-        # split tokens into a list
-        tokens = tokens.split(operator)
-        # convert the values from string to floats
-        values = [d_type(i) for i in tokens]
-        return values
+    def clear_operator_tokens(self, tokens, operator, d_type=float):
+        new_cleaned_list = []
+        for e in tokens:
+            if e != operator:
+                new_cleaned_list.append(e)
+        print("new_cleaned_list", new_cleaned_list)
+        return new_cleaned_list
 
     @classmethod
     def process_brackets(self, tokens):
@@ -31,21 +37,21 @@ class CalEngine(object):
         for e in tokens:
             if e == "(" or e == ")":
                 indexes.append(tokens.index(e))
-        
+
         if len(indexes) % 2 != 0:
             # raise error
             return {"error": "Invalid Input, Incomplete Brackets"}
-        
+
         # check there are no brackets in the whole equation
         if len(indexes) == 0:
             result = self.calculate_equation(tokens)
-            print("result", result)
+            print("result_one", result)
             return result
-        
+
         return "done"
-        
+
         # get starting inner brackets
-        start = (len(indexes)/2) - 1
+        start = (len(indexes) / 2) - 1
         # get ending inner brackets
         end = start + 1
         bracket_tokens = tokens[start:end]
@@ -57,59 +63,60 @@ class CalEngine(object):
         return self.process_brackets(tokens)
 
     @classmethod
-    def edge_detection(self, tokens, operator, operator_index, result):
-        # check if either of the tokens before or after the operator are at the end of the equation
-        if tokens.index(tokens[operator_index-1]) <= 0 or tokens.index(tokens[operator_index+1]) >= len(tokens):
-            # get token before operators left value
-            tokens_before_operator = tokens[:operator_index]
-            # get token after operators right value
-            tokens_after_operator = tokens[operator_index+1:]
+    def edge_detection(self, tokens, operator_index, result):
+        # get token before operators left value
+        tokens_before_operator = tokens[:operator_index]
+        # get token after operators right value
+        tokens_after_operator = tokens[operator_index + 1 :]
+        print("tokens_before_operator", tokens_before_operator)
+        print("tokens_after_operator", tokens_after_operator)
+        print("tokens_@_edge_detection", tokens)
+        print("result-two", result)
+        print("====================================")
+        # check if either of the values on the sode are the only values in the equation
+        if len(tokens_before_operator) <= 1 and len(tokens_after_operator) > 1:
+            # update the token_after variable as slicing works differently on the right side
+            new_tokens_after_operator = tokens[operator_index + 2 :]
             # concatenate the tokens with the result in the middle
-            new_tokens = f"{tokens_before_operator}{result}{tokens_after_operator}"
-            print("before", tokens.index(tokens[:operator_index]))
-            print("new_tokens", new_tokens)
-            print("tokens", tokens)
-            print("tokens_before_operator", tokens_before_operator)
-            print("tokens_after_operator", tokens_after_operator)
-            return "done"
+            new_tokens = f"{result}{new_tokens_after_operator}"
+            print("new_tokens_i", new_tokens)
             return self.calculate_equation(new_tokens)
-        elif tokens.index(tokens[:operator_index]) == 0:
-            # get token after operators right value
-            tokens_after_operator = tokens[operator_index+1:]
-            # concatenate the tokens with the result in the middle
-            new_tokens = f"{result}{tokens_after_operator}"
-            return self.calculate_equation(new_tokens)
-        elif tokens.index(tokens[operator_index+1:]) == len(tokens):
-            # get token before operators left value
-            tokens_before_operator = tokens[:operator_index]
+        elif len(tokens_after_operator) <= 1 and len(tokens_before_operator) > 1:
             # concatenate the tokens with the result in the middle
             new_tokens = f"{tokens_before_operator}{result}"
+            print("new_tokens_ii", new_tokens)
+            return self.calculate_equation(new_tokens)
+        elif len(tokens_after_operator) > 1 and len(tokens_before_operator) > 1:
+            # update the token_after variable as slicing works differently on the right side
+            new_tokens_after_operator = tokens[operator_index + 2 :]
+            # concatenate the tokens with the result in the middle
+            new_tokens = f"{tokens_before_operator}{result}{new_tokens_after_operator}"
+            print("new_tokens_iii", new_tokens)
             return self.calculate_equation(new_tokens)
         else:
-            new_tokens = result
-
-        # check if there are any more plus(+) operators
-            if operator in new_tokens:
-                # recursively call the function
-                return self.calculate_equation(new_tokens)
-            else:
-                return new_tokens
+            return result
 
     @classmethod
     def operator_detection(self, tokens, operator):
         # get operator sign index
         operator_index = tokens.index(operator)
-        # fetch tokens before and after the operator
-        associated_tokens = f"{tokens[operator_index-1]}{operator}{tokens[operator_index+1]}"
-        result = self.addition(associated_tokens)
-        # detect edges
-        self.edge_detection(tokens, operator, operator_index, result)
+        print("operator_index-operator_detection", operator_index)
 
+        # fetch tokens before and after the operator
+        associated_tokens = [tokens[operator_index-1], operator, tokens[operator_index+1]]
+        print("====================================")
+        print("tokens", tokens)
+        print("associated_tokens", associated_tokens)
+        result = self.addition(associated_tokens)
+        # print("operator_part_result", result)
+        print("====================================")
+
+        # detect edges
+        return self.edge_detection(tokens, operator_index, result)
 
     @classmethod
     def calculate_equation(self, tokens):
         if "+" in tokens:
-            print("additon detected", tokens)
             return self.operator_detection(tokens, operator="+")
         # elif "-" in tokens:
         #     print("subtraction detected", tokens)
@@ -122,22 +129,19 @@ class CalEngine(object):
         #     return self.operator_detection(tokens, operator="/")
         else:
             return tokens
-            
 
     @classmethod
     def addition(self, tokens):
-        # split tokens into a list and convert to floats
-        values = self.convert_tokens(tokens, "+")
-        # print("values", values)
+        # clean list of all operators
+        values = self.clear_operator_tokens(tokens, "+")
+        print("values---", values)
         total = sum(values)
-        print("values", values)
-        print("total", total)
-        return total
+        return round(total, 2)
 
     @classmethod
     def multiplication(self, tokens):
         # split tokens into a list and convert to floats
-        values = self.convert_tokens(tokens, "*")
+        values = self.clear_operator_tokens(tokens, "*")
         total = values[0]
         for i in values[1:]:
             total *= i
@@ -146,7 +150,7 @@ class CalEngine(object):
     @classmethod
     def division(self, tokens):
         # split tokens into a list and convert to floats
-        values = self.convert_tokens(tokens, "/")
+        values = self.clear_operator_tokens(tokens, "/")
         total = values[0]
         for i in values[1:]:
             total /= i
@@ -155,13 +159,11 @@ class CalEngine(object):
     @classmethod
     def subtraction(self, tokens):
         # split tokens into a list and convert to floats
-        values = self.convert_tokens(tokens, "-")
+        values = self.clear_operator_tokens(tokens, "-")
         total = values[0]
         for i in values[1:]:
             total -= i
         return total
-
-
 
 
 # BODMAS
@@ -174,6 +176,6 @@ class CalEngine(object):
 # S - Subtraction - 2-3
 
 cal_engine = CalEngine()
-cleaned_tokens = CalEngine.clean_input("3+4")
+cleaned_tokens = CalEngine.clean_input("3+4+1")
 solve_brackets = CalEngine.process_brackets(cleaned_tokens)
 print(solve_brackets)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-tokens = "0+4"
+tokens = "0+4+3"
 # loop through the string
 # if the character is a ( or a ) then add it to a list
 # if the character is not a ( or a ) then add it to a string
@@ -7,13 +7,27 @@ tokens = "0+4"
 # for e in tokens:
 #     print(e, tokens.index(e))
 
-    # if e == "(" or e == ")":
-    #     print(e)
-    # else:
-    #     print("not a parenthesis")
+# if e == "(" or e == ")":
+#     print(e)
+# else:
+#     print("not a parenthesis")
 
 # for e in tokens
 
 operator = "+"
-print(tokens.index(tokens[2]))
-print(tokens.index(tokens[:2]))
+print(tokens[2:])
+print(tokens[:2])
+# print(tokens.index(tokens[2:]))
+# print(tokens.index(tokens[:2]))
+
+
+# NOTE: left side doesnt count from the pointed index
+# tokens = "0+4+3"
+# the above tokens will result for left side as
+# 0+ (instead of 0+4)
+# while right side will be
+# 4+3
+
+
+n = 7.0
+print(float(n))
